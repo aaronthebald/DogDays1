db4bcad0eff85dbe8ea1c4ceb4c217942ac9d646
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2023-10-27 19:51:22 -0400

    Actions: Built new EventCard view with SwiftUI. This view is intended to be used in a List structure in the HomeView. This replaces the eventGrid view. Removed all old UI code for the EventGrid layout.
    Reasoning:
    I believe users are more familiar and thus more comfortable with a list that they can swipe on to reveal action buttons such as edit and delete.

---
+++ DogDays/Core/Views/EventCard.swift
@@ -0,0 +1,18 @@
+//
+//  EventCard.swift
+//  DogDays
+//
+//  Created by Aaron Wilson on 10/26/23.
+//
+
+import SwiftUI
+
+struct EventCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventCard()
+}
