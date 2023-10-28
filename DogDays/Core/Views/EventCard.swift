5fb4c4a56d106754f2ed52517b24ae420ac1aadf
Aaron Wilson <124102001+aaronthebald@users.noreply.github.com>
2023-10-28 15:44:14 -0400

    Actions: Built new EventCard view with SwiftUI. This view is intended to be used in a List structure in the HomeView. This replaces the eventGrid view. Removed all old UI code for the EventGrid layout.
    Reasoning:
    I believe users are more familiar and thus more comfortable with a list that they can swipe on to reveal action buttons such as edit and delete.

--- DogDays/Core/Views/EventCard.swift
+++ DogDays/Core/Views/EventCard.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct EventCard: View {
+    let item: EventEntity
+    @ObservedObject var vm: HomeViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.title ?? "Event has no title")
+                    .font(.headline)
+                Text(item.location ?? "Event has no Location")
+                    .font(.subheadline)
+                Text(item.date?.formatted(date: .abbreviated, time: .shortened) ?? "Event has no date")
+            }
+            Spacer()
+            Image(systemName: vm.getImage(event: item))
+                .font(.title)
+                .foregroundColor(Color.theme.accent)
+        }        
     }
 }
 
-#Preview {
-    EventCard()
-}
+
